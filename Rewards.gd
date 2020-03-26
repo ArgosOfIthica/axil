7726ffccea79fdf579b8f4d84aa6638929a212db
RileyStetler <rbstet@gmail.com>
2020-03-26 14:51:02 -0400

    Hide Rewards nodes

--- Rewards.gd
+++ Rewards.gd
@@ -6,17 +6,20 @@ var nutrient = null
 signal open_choose
 
 func goto_rewards():
+	#prepare_rewards_rendering()
 	show()
-#	random_resources(water)
-#	random_resources(nutrient)
 	$PU.get_child(0).show()
 
-func ready():
+func _ready():
 	hide()
 
 #func random_resources(resource):
 #	var rng = randi()%4+1
 #	return rng.resource
+
+#func prepare_rewards_rendering():
+#	random_resources(water)
+#	#random_resources(nutrient)
 
 func _on_Button_pressed():
 	emit_signal("open_choose")
