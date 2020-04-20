6a3597b19cd75de79213303a6b902efb191fe747
gwhult <ghultquist@bsu.edu>
2020-04-20 10:51:01 -0400

    Add title screen and work on encyclopedia

--- Encyclopedia.gd
+++ Encyclopedia.gd
@@ -17,7 +17,49 @@ func _ready():
 
 #water/nutrients --> plant.water/3600 + "hours"
 const notes = {
-	"fire" : ["very hot", "dummy hot", "gotdang that hot"]
+	"fire" : ["very hot", "dummy hot", "dang that hot"],
+	"water": [],
+	"grass": [],
+	"spoon": [],
+	"redspider": [],
+	"digiveg": [],
+	"kniveleaves": [],
+	"livingblub": [],
+	"potatocoon": [],
+	"stonemoss": [],
+	"purpletentacles": [],
+	"firefleur" : [],
+	"hand": [],
+	"alienboi": [],
+	"strangerock" : [],
+	"smileyboi" : [],
+	"strugglepot" : [],
+	"stitchy" : [],
+	"ice" : [],
+	"rainbowshine": []
+	}
+	
+const collection = {
+	"fire" : 0,
+	"water": 0,
+	"grass": 0,
+	"spoon": 0,
+	"redspider": 0,
+	"digiveg": 0,
+	"kniveleaves": 0,
+	"livingblub": 0,
+	"potatocoon": 0,
+	"stonemoss": 0,
+	"purpletentacles": 0,
+	"firefleur" : 0,
+	"hand": 0,
+	"alienboi": 0,
+	"strangerock" : 0,
+	"smileyboi" : 0,
+	"strugglepot" : 0,
+	"stitchy" : 0,
+	"ice" : 0,
+	"rainbowshine": 0
 	}
 
 func _on_backbutton_pressed():
